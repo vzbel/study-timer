ecdbf7f1b76689cb7cc895d8322339374a8a1718
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-01 12:47:46 -0600

    Implement timer display modification function

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 class Timer {
   // HTML properties
   #buttonsContainer = document.querySelector(".timer-buttons-container"); // The timer's container from the HTML file
+  #display = document.querySelector(".timer-output"); // The timer output HTML element
 
   // Timer properties
   #minutes; // Current minutes
@@ -27,7 +28,7 @@ class Timer {
   handleButtonPress(dataAction) {
     switch (dataAction) {
       case "start":
-        console.log("start");
+        this.start(); // Start timer
         break;
       case "stop":
         console.log("stop");
@@ -37,7 +38,32 @@ class Timer {
         break;
     }
   }
+
+  // Start timer on button press
+  //   start() {
+  //     // Start timer interval, which decreases the time every second.
+  //     let interval = setInterval(() => {
+  //       this.#seconds--;
+  //       this.updateDisplay();
+  //     }, 1000);
+  //   }
+
+  // Update timer display
+  updateDisplay() {
+    // If the minutes and seconds (or either one) are less than ten,
+    // Display an extra zero to the left of the corresponding value.
+    // Otherwise, if none are less than ten, display them as is.
+    if (this.#minutes < 10 && this.#seconds < 10) {
+      this.#display.textContent = `0${this.#minutes}:0${this.#seconds}`;
+    } else if (this.#minutes < 10) {
+      this.#display.textContent = `0${this.#minutes}:${this.#seconds}`;
+    } else if (this.#seconds < 10) {
+      this.#display.textContent = `${this.#minutes}:0${this.#seconds}`;
+    } else {
+      this.#display.textContent = `${this.#minutes}:${this.#seconds}`;
+    }
+  }
 }
 
 // Create main 30 minute timer
-const timer = new Timer(30, 0);
+const timer = new Timer(18, 58);
